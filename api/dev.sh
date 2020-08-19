1f2c1003a1f16833f81cb2bd630c84a97cbb5dc6
Samuel Ainsworth <skainsworth@gmail.com>
2020-08-19 15:41:47 -0700

    fix dev.sh so it doesn't constantly recompile everything

--- api/dev.sh
+++ api/dev.sh
@@ -1,9 +1,12 @@
 # Pull in all the env vars from /etc/cuddlefish-dev.env
 set -a; source /etc/cuddlefish-dev.env; set +a
 
+# See https://doc.rust-lang.org/std/backtrace/index.html#environment-variables.
 MIRRORS_DIR=mirrors \
-    RUSTFLAGS=-Awarnings \
     RUST_LIB_BACKTRACE=1 \
     RUST_BACKTRACE=full \
     RUST_LOG=api=trace \
     cargo watch -x run
+
+# This flag causes cargo to recompile everything every single time you save.
+# RUSTFLAGS=-Awarnings \
