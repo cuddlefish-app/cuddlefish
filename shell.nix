d0b20d391127f1b103d882b8f2b8fd105f8118eb
Samuel Ainsworth <skainsworth@gmail.com>
2021-03-05 01:14:11 +0000

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 let
-  # Last updated: 2/26/21
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/04ac9dcd311956d1756d77f4baf9258392ee7bdd.tar.gz")) {};
+  # Last updated: 3/4/21
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/5df05c902cde398e056eb6271d5fe13e418db4c6.tar.gz")) {};
 
   # Rolling updates, not deterministic.
   # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
@@ -20,9 +20,6 @@ in pkgs.mkShell {
     pkgs.openssl
     pkgs.pkg-config
   ];
-  shellHook = ''
-    yarn --cwd web install --frozen-lockfile
-  '';
 
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
