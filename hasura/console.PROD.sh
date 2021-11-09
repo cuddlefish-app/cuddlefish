46a7e5515f68127c221a94a345d09a4f8f756e68
Samuel Ainsworth <skainsworth@gmail.com>
2021-11-09 00:34:16 -0800

    hasura: create console.PROD.sh script

---
+++ hasura/console.PROD.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -a; source /etc/cuddlefish-PROD.env; set +a
+echo "===== CAUTION ====="
+echo "Never under any circumstance make changes to the schema/metadata in this console!"
+echo "==================="
+hasura console --endpoint=https://hasura.cuddlefish.app/
