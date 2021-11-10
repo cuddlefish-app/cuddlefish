83fcb910cad98074cc4bdf9e8a3b11773ea201bd
Samuel Ainsworth <skainsworth@gmail.com>
2021-11-10 14:43:33 -0800

    hasura: add TODO in console.PROD.sh re --envfile option

--- hasura/console.PROD.sh
+++ hasura/console.PROD.sh
@@ -4,3 +4,5 @@ echo "===== CAUTION ====="
 echo "Never under any circumstance make changes to the schema/metadata in this console!"
 echo "==================="
 hasura console --endpoint=https://hasura.cuddlefish.app/
+
+# TODO use env file and point hasura console towards that --envfile!
