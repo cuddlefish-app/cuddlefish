52af951a21cca5235abc781e16b7d1dc1e4c17d7
Samuel Ainsworth <skainsworth@gmail.com>
2021-11-11 13:39:49 -0800

    hasura: put prod console onto a different port than dev console

--- hasura/console.PROD.sh
+++ hasura/console.PROD.sh
@@ -3,6 +3,6 @@ set -a; source /etc/cuddlefish-PROD.env; set +a
 echo "===== CAUTION ====="
 echo "Never under any circumstance make changes to the schema/metadata in this console!"
 echo "==================="
-hasura console --endpoint=https://hasura.cuddlefish.app/
+hasura console --endpoint=https://hasura.cuddlefish.app/ --console-port=11111
 
 # TODO use env file and point hasura console towards that --envfile!
