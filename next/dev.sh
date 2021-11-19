6b0264d6328fa9459974dea00408463261738854
Samuel Ainsworth <skainsworth@gmail.com>
2021-11-18 16:03:11 -0800

    next: unnecessary `--` according to yarn

--- next/dev.sh
+++ next/dev.sh
@@ -1,7 +1,7 @@
 # Pull in all the env vars from /etc/cuddlefish-dev.env
 set -a; source /etc/cuddlefish-dev.env; set +a
 
-yarn run gql:codegen -- --watch &
+yarn run gql:codegen --watch &
 
 # See also .env.local
 yarn run dev
