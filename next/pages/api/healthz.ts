7f5909209bef3a4dfacecaf4c3fd4aa9378abd8c
Samuel Ainsworth <skainsworth@gmail.com>
2021-11-18 17:27:04 -0800

    next: refactor /api/healthz and also return process.versions

--- next/pages/api/healthz.ts
+++ next/pages/api/healthz.ts
@@ -8,6 +8,36 @@ import {
   logHandlerErrors,
 } from "../../src/server/utils";
 
+// Put individual checks into their own functions so that it's easier to
+// identify what's failing from stack traces.
+
+async function checkSendgrid() {
+  // Confirm that our Sendgrid API key is valid. `setApiKey` sadly doesn't do this for us.
+  // See https://stackoverflow.com/questions/61658558/how-to-test-sendgrid-api-key-is-valid-or-not-without-sending-emails
+  // TODO use https://stackoverflow.com/questions/56074531/how-to-retry-5xx-requests-using-axios for retries
+  await axios.get("https://api.sendgrid.com/v3/scopes", {
+    headers: {
+      Authorization: `Bearer ${notNull(process.env.SENDGRID_API_KEY)}`,
+    },
+  });
+}
+
+async function checkHasura() {
+  // Confirm that we can reach the Hasura GraphQL endpoint
+  const hasuraResponse = await ADMIN_buildApolloClient().query({
+    query: gql`
+      query {
+        __typename
+      }
+    `,
+  });
+  assert(hasuraResponse.error === undefined, "expected no error");
+  assert(
+    hasuraResponse.errors === undefined || hasuraResponse.errors.length === 0,
+    "expected no error"
+  );
+}
+
 export default logHandlerErrors(async (req: NextApiRequest) => {
   assert400(req.method === "GET", "GET only");
 
@@ -22,31 +52,11 @@ export default logHandlerErrors(async (req: NextApiRequest) => {
   notNull(process.env.SENDGRID_API_KEY, "expected SENDGRID_API_KEY");
   notNull(process.env.GITHUB_API_TOKEN, "expected GITHUB_API_TOKEN");
 
-  // TODO check that GITHUB_API_TOKEN is still valid.
-
-  // Confirm that our Sendgrid API key is valid. `setApiKey` sadly doesn't do this for us.
-  // See https://stackoverflow.com/questions/61658558/how-to-test-sendgrid-api-key-is-valid-or-not-without-sending-emails
-  await axios.get("https://api.sendgrid.com/v3/scopes", {
-    headers: {
-      Authorization: `Bearer ${notNull(process.env.SENDGRID_API_KEY)}`,
-    },
-  });
-
-  // Confirm that we can reach the Hasura GraphQL endpoint
-  const hasuraResponse = await ADMIN_buildApolloClient().query({
-    query: gql`
-      query {
-        __typename
-      }
-    `,
-  });
-  assert(hasuraResponse.error === undefined, "expected no error");
-  assert(
-    hasuraResponse.errors === undefined || hasuraResponse.errors.length === 0,
-    "expected no error"
-  );
-
+  // TODO check that GITHUB_API_TOKEN is valid
   // TODO check that we reach the Hasura /healthz endpoint
 
-  return { NODE_ENV: process.env.NODE_ENV };
+  await checkSendgrid();
+  await checkHasura();
+
+  return { NODE_ENV: process.env.NODE_ENV, processVersions: process.versions };
 });
